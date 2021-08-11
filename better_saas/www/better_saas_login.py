c4689f37efda9c47d06598a08bf8668027abd328
Narendra Yadav <narendra@onehash.ai>
2021-08-11 18:34:04 +0530

    Update AddOn Services Auto Allocation

--- better_saas/www/better_saas_login.py
+++ better_saas/www/better_saas_login.py
@@ -4,6 +4,6 @@ import frappe
 def fetch_site_by_email(email):
     try:
         domains = list(set([x.get("parent") for x in frappe.get_list('User Details', filters={'emai_id': email, 'active': 1}, fields=['parent'])]))
-        return ["app.onehash.ai"] + domains
+        return [frappe.conf.get("master_site_domain")] + domains
     except:
         return []
