4b4ee23ed33eacc11885f36b5d2ccf2b1f0b07ce
Narendra Yadav <narendra@onehash.ai>
2022-01-27 22:56:13 +0530

    Coupon Coode Limit

---
+++ better_saas/better_saas/doctype/coupon_code_stack_limit/coupon_code_stack_limit.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2022, Vigneshwaran Arumainayagam and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+class CouponCodeStackLimit(Document):
+	pass
