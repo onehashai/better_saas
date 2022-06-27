8d8c28c2e5fc58b7b2020c0b570727ec28cd7346
Narendra Yadav <narendra@onehash.ai>
2022-06-27 17:54:31 +0530

    Update Grace Period, Subscription Bug Fix

--- better_saas/better_saas/doctype/saas_settings/saas_settings.py
+++ better_saas/better_saas/doctype/saas_settings/saas_settings.py
@@ -5,6 +5,18 @@
 from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
+from frappe.utils import today, nowtime
 
 class SaasSettings(Document):
-	pass
+	def on_update(self):
+		commands =[]
+		if self.grace_period:
+			commands.append("bench set-config -g grace_period {0}".format(self.grace_period))
+		if self.instafinancial_key:
+			commands.append("bench set-config -g insta_financial_api_key {0}".format(self.instafinancial_key))
+		frappe.enqueue('bench_manager.bench_manager.utils.run_command',
+			commands=commands,
+			doctype="Bench Settings",
+			key=today() + " " + nowtime(),
+			now = True)
+		pass
